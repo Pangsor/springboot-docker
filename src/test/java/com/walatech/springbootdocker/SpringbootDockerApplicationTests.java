bcdeaac3a7f5792c013f5ca5035078f85cacb188
Septiyana Pangsor <septiyana.pangsor@ifg-life.id>
2023-08-23 14:31:33 +0700

    springboot dockerizing

---
+++ src/test/java/com/walatech/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walatech.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
